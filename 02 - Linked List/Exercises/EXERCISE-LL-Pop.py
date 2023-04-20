ef8239256a9ee223a7c7435d2b843270c6d073e5
Chrisovalantis Vatos <chrisovalantis.vatos@mail.mcgill.ca>
2023-04-20 08:57:10 -0400

    Finished LL exercises and started LL intreview questions

--- 02 - Linked List/Exercises/EXERCISE-LL-Pop.py
+++ 02 - Linked List/Exercises/EXERCISE-LL-Pop.py
@@ -31,11 +31,20 @@ class LinkedList:
     ### WRITE POP METHOD HERE ###
     #############################
     def pop(self):
-        tmp = self.head
-        
-
- 
-
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
 
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
